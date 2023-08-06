4eee365974a252741006aa453f5f8548e5d7fa8c
OmAxiani0 <aximaniom@gmail.com>
2023-08-06 15:29:02 +0530

    Connected to database
    - changed logs path

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { connectToDB } from "./db";
 
 if (!process.env.PORT) {
     process.exit(-1);
@@ -9,6 +10,9 @@ if (!process.env.PORT) {
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app: Express = express();
+
+// Connect to database
+connectToDB();
 
 // Middleware section
 app.use(helmet());
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -11,8 +11,8 @@ const loggerOptions: LoggerOptions = {
     format: format.cli(),
     transports: [
         new transports.Console(),
-        new transports.File({ filename: "error.log", level: "error" }),
-        new transports.File({ filename: "combined.log" }),
+        new transports.File({ filename: "logs/error.log", level: "error" }),
+        new transports.File({ filename: "logs/combined.log" }),
     ],
 };
 
