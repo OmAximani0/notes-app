cf2f9f08778aaedd0cedcf4c85ca3007284e71c5
OmAxiani0 <aximaniom@gmail.com>
2023-08-11 22:05:27 +0530

    Notes feature completed
    Signed-off-by: OmAxiani0 <aximaniom@gmail.com>

--- src/services/note.service.ts
+++ src/services/note.service.ts
@@ -59,7 +59,10 @@ export async function createNote(req: Request, res: Response) {
         return res.status(400).json(response);
     }
 
-    const note = await Note.findOne({ title: req.body.title, user: req.user?._id });
+    const note = await Note.findOne({
+        title: req.body.title,
+        user: req.user?._id,
+    });
     if (note) {
         response["error"] = ["Note title is already taken"];
         return res.status(401).json(response);
@@ -96,9 +99,87 @@ export async function createNote(req: Request, res: Response) {
 }
 
 export async function updateNote(req: Request, res: Response) {
-    res.send("Update Note");
+    let response: CustomResponseExtended = {};
+
+    checkFields(req.body, ["noteId", "note"], response);
+    if (response.error) {
+        return res.status(400).json(response);
+    }
+
+    const noteId =
+        typeof req.body.noteId === "string"
+            ? req.body.noteId
+            : String(req.body.noteId);
+
+    if (Types.ObjectId.isValid(noteId)) {
+        try {
+            const note = await Note.findOneAndUpdate(
+                {
+                    _id: noteId,
+                    user: req.user?._id,
+                },
+                {
+                    ...req.body.note,
+                },
+                {
+                    new: true,
+                    fields: [
+                        "-_id",
+                        "-user",
+                        "-__v"
+                    ]
+                }
+            );
+
+            if (note) {
+                response["message"] = "Note updated successfully!";
+                response["note"] = note;
+                return res.status(200).json(response);
+            }
+
+            response["error"] = ["Note not found"];
+            return res.status(404).json(response);
+        } catch (error: any) {
+            response["error"] = [error.message];
+            return res.status(500).json(response);
+        }
+    }
+
+    response["error"] = ["Invalid note id"];
+    return res.status(400).json(response);
 }
 
 export async function deleteNote(req: Request, res: Response) {
-    res.send("Delete Note");
+    let response: CustomResponse = {};
+
+    checkFields(req.body, ["noteId"], response);
+    if (response.error) {
+        return res.status(400).json(response);
+    }
+
+    let noteId =
+        typeof req.body.noteId === "string"
+            ? req.body.noteId
+            : String(req.body.noteId);
+
+    if (Types.ObjectId.isValid(noteId)) {
+        try {
+            const note = await Note.findOneAndDelete({
+                _id: noteId,
+                user: req.user?._id,
+            });
+            if (note) {
+                response["message"] = "Note deleted successfully!";
+                return res.status(200).json(response);
+            }
+            response["error"] = ["Note not found!"];
+            return res.status(404).json(response);
+        } catch (error: any) {
+            response["error"] = [error.message];
+            return res.status(500).json(response);
+        }
+    }
+
+    response["error"] = ["Invalid note id!"];
+    return res.status(400).json(response);
 }
